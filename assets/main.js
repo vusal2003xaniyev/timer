4a033c69565d0abc0f2a81753d5132e94ea5414c
vusal2003xaniyev <xaniyevvusal@gmail.com>
2023-09-19 16:14:43 +0400

    edit

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,11 @@ var stopButton = document.querySelector("#stop");
 var hourTime = document.querySelector("#hour");
 var minuteTime = document.querySelector("#minute");
 var secondTime = document.querySelector("#seconds");
+var back = document.querySelector("#back");
+var forward = document.querySelector("#forward");
+var forwardButton = document.querySelector("#button");
+var okay = document.querySelector("#okay");
+
 var hour = 0;
 var minute = 0;
 var second = 0;
@@ -36,15 +41,16 @@ startButton.onclick = function () {
       }
       hourTime.innerHTML = hour;
     }
+    if(start_stop==true){
+        clearInterval(timer);
+    }
+    console.log(start_stop);
   }, 10);
   if (start_stop) {
     document.getElementById("start").innerHTML = "Stop";
     start_stop = false;
     console.log("if");
-
   } else {
-    clearInterval(timer);
-
     document.getElementById("start").innerHTML = "Start";
     console.log("else");
     console.log(timer);
@@ -66,4 +72,44 @@ breakButton.onclick = function () {
       breakButton.style.backgroundColor = "rgb(119, 118, 118)";
     }, 100);
   }
+};
+back.onclick = function () {
+  forwardButton.style.display = "none";
+  document.getElementById("backButton").style.display = "block";
+  document.getElementById("result").style.display = "none";
+};
+forward.onclick = function () {
+  forwardButton.style.display = "flex";
+  document.getElementById("backButton").style.display = "none";
+  document.getElementById("result").style.display = "block";
+};
+okay.onclick = function () {
+  var hourTimer = document.getElementById("timer_hour").value;
+  var minuteTimer = document.getElementById("timer_minute").value;
+  var secondTimer = document.getElementById("timer_second").value;
+  console.log(`${hourTimer} : ${minuteTimer} : ${secondTimer}`);
+  if (hourTimer != 0 || minuteTimer != 0 || secondTimer != 0) {
+    var time=setInterval(function () {
+      if (secondTimer == 0) {
+        minuteTimer--;
+        secondTimer = 59;
+      }
+
+      secondTimer--;
+      secondTime.innerHTML = secondTimer;
+      if (hourTimer > 0) {
+        if (minuteTimer == 0 && secondTimer == 0) {
+          minuteTimer = 60;
+          hourTimer--;
+        }
+      }
+      minuteTime.innerHTML = minuteTimer;
+      hourTime.innerHTML = hourTimer;
+      if (hourTimer == 0 && minuteTimer == 0 && secondTimer == 0) {
+        clearInterval(time);
+        alert("Zaman bitdi");
+      }
+    }, 10);
+  }
+ 
 };
